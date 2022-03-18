cd4c4713c27347f9c8317377f6ab778c11650404
Redouane Kachach <rkachach@redhat.com>
2022-03-18 20:21:47 +0100

    emacs: fixing projectile configuration
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>

--- .emacs.d/init-loader/10-projectile.el
+++ .emacs.d/init-loader/10-projectile.el
@@ -5,5 +5,9 @@
   :bind (("C-c C-f" . helm-find-files))
   :config
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (setq projectile-indexing-method 'alien)
+  (setq projectile-sort-order 'recentf)
+  (setq projectile-enable-caching nil)
+  (setq projectile-completion-system 'helm)
   (helm-projectile-on)
   (projectile-mode))
