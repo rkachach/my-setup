78cdc2e9c117611c8a82edcaa85c9a42893cb04a
Redouane Kachach <rkachach@redhat.com>
2022-03-22 10:16:14 +0100

    emacs: removing unused conf from python mode
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>

--- .emacs.d/init-loader/15-python-mode.el
+++ .emacs.d/init-loader/15-python-mode.el
@@ -3,14 +3,4 @@
   :bind
   ( :map python-mode-map
 	 ("C-c C-r" . python-indent-shift-right)
-	 ("C-c C-l" . python-indent-shift-left))
-  :hook
-  ;; With pyls:
-  ;; pip install python-language-server flake8 pyls-black(optional) pyls-isort(optional)
-  ;; With pyright
-  ;; sudo npm install -g pyright && pip install flake8 black(optional)
-  ;; NOTE: these hooks runs in reverse order
-  (python-mode . (lambda () (setq-local company-prescient-sort-length-enable nil)))
-  (python-mode . (lambda () (setq-local fill-column 79)))
-  :config
-  )
+	 ("C-c C-l" . python-indent-shift-left)))
