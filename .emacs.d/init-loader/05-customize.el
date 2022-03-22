fe31a1e52a654ab92acc3384869eb40829b0b456
Redouane Kachach <rkachach@redhat.com>
2022-03-22 10:17:19 +0100

    emacs: updating gitignore
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>

--- .emacs.d/init-loader/05-customize.el
+++ .emacs.d/init-loader/05-customize.el
@@ -4,11 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-pyright helm-lsp helm-flyspell flycheck-mypy magit dired-efap yasnippet lsp-ui which-key lsp-python-ms lsp-mode diff-hl company-tabnine company-quickhelp company-anaconda anaconda-mode company ethan-wspace powerline helm-core projectile-ripgrep projectile monokai-theme init-loader use-package))
+   '(helm-flyspell flycheck-mypy magit dired-efap yasnippet lsp-ui which-key lsp-mode diff-hl company-tabnine company-quickhelp company-anaconda anaconda-mode company ethan-wspace powerline helm-core projectile-ripgrep projectile monokai-theme init-loader use-package))
  '(projectile-enable-caching t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(iedit-occurrence ((t (:inherit highlight :background "orange red"))))
+ '(isearch ((t (:background "yellow" :foreground "forest green"))))
+ '(lazy-highlight ((t (:background "yellow" :foreground "forest green" :inverse-video nil)))))
