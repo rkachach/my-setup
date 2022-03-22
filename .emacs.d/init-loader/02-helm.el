da9181de384da9f23d6cfb879f3082bff5ab4617
Redouane Kachach <rkachach@redhat.com>
2022-03-22 10:14:18 +0100

    emacs: fixing helm fuzzy configuration
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>

--- .emacs.d/init-loader/02-helm.el
+++ .emacs.d/init-loader/02-helm.el
@@ -9,12 +9,11 @@
 	 ("M-x" . helm-M-x)
          ("C-x C-f" . helm-find-files)
          ("C-x b" . helm-buffers-list)
-         ("C-x c o" . helm-occur))
-         ("M-y" . helm-show-kill-ring)
-         ("C-x r b" . helm-filtered-bookmarks)
+         ("C-x c o" . helm-occur)
+	 ("C-x r b" . helm-filtered-bookmarks)
 	 ("C-x C-y" . 'helm-show-kill-ring)
 	 ("C-x y" . 'helm-flyspell-correct)
-	 ("C-x C-n" . 'helm-swoop)
+	 ("C-x C-n" . 'helm-swoop)) ;; TODO: fixme not defined
 
   :preface (require 'helm-config)
   :config
@@ -22,7 +21,22 @@
 	helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
 	helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
 	helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-	helm-ff-file-name-history-use-recentf t)
+	helm-M-x-fuzzy-match t
+	helm-apropos-fuzzy-match t
+	helm-ff-file-name-history-use-recentf t
+	helm-buffers-fuzzy-matching t
+	helm-completion-in-region-fuzzy-match t
+	helm-etags-fuzzy-match t
+	helm-ff-file-name-history-use-recentf t
+	helm-ff-search-library-in-sexp t
+	helm-file-cache-fuzzy-match t
+	helm-imenu-fuzzy-match t
+	helm-lisp-fuzzy-completion t
+	helm-locate-fuzzy-match t
+	helm-mode t
+	helm-mode-fuzzy-match t
+	helm-move-to-line-cycle-in-source t
+	helm-recentf-fuzzy-match t)
   (helm-adaptive-mode t)
   (helm-mode 1))
 
